f3a3d39fdd4ef298b34ae2af22132a16aabea0f8
Mykhailo Popyzenko <mp.and.sp@ukr.net>
2023-04-25 23:50:19 +0300

    update

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,8 +1,9 @@
 const BASE_URL = 'https://restcountries.com/v3.1';
 
 function fetchCountries(name) {
-    return fetch(`${BASE_URL}/name/${name}?fields=name,capital,population,flags,languages`)
-        .then(response => response.json());
+  return fetch(
+    `${BASE_URL}/name/${name}?fields=name,capital,population,flags,languages`
+  ).then(response => response.json());
 }
 
-export default { fetchCountries } ;
+export default { fetchCountries };
--- src/index.js
+++ src/index.js
@@ -18,47 +18,71 @@ function onSearch(e) {
 
   const searchCountry = refs.input.value.trim();
 
-    API.fetchCountries(searchCountry)
-        // .then(countryInfoCard)
-        .then()
-        .catch(onFetchError);
-  // .finally(() => searchCountry.reset());
+  API.fetchCountries(searchCountry)
+    .then(fetchManyCoutries)
+    .then(countriesList)
+    .then(countryInfoCard)
+    .catch(onFetchError);
 }
 
-function countryInfoCard(name) {
-  const markup = name
-    .map(({ name, capital, population, flags, languages }) => {
-      return `
+function countryInfoCard(countries) {
+  if (countries.length === 1) {
+    const markup = countries
+      .map(({ name, capital, population, flags, languages }) => {
+        return `
       <div class="country__card">
         <div>
           <img class="country__flag"
             src="${flags.svg}"
             alt="flag of ${name.official}"
-            width="25"
+            width="35"
             />
           <h2 class="country__name">${name.official}</h2>
         </div>
-        <p class="country__capital">Столиця:
+        <p class="country__capital">Capital:
           <span class="capital">${capital}</span>
         </p>
-        <p class="country__population">Населення:
+        <p class="country__population">Population:
           <span class="population">${population}</span>
         </p>
-        <p class="country__language">Мова:
+        <p class="country__language">Languages:
           <span class="language">${Object.values(languages)}</span>
         </p>
       </div>
-    `;
-    })
-    .join('');
-  refs.countryInfo.innerHTML = markup;
-  console.log(markup);
+      `;
+      })
+      .join('');
+    refs.countryInfo.innerHTML = markup;
+    console.log(markup);
+  }
+}
+
+function countriesList(countries) {
+  if (countries.length <= 10) {
+    const markup = countries
+      .map(({ name, flags }) => {
+        return `
+      <li>
+        <img class="country__flag"
+          src="${flags.svg}"
+          alt="flag of ${name.official}"
+          width="35"
+          />
+        <h2 class="country__name">${name.official}</h2>
+      </li>
+      `;
+      })
+      .join('');
+    refs.countryList.innerHTML = markup;
+  }
+}
+
+function fetchManyCoutries(countries) {
+  if (countries.length > 10) {
+    Notify.info('Too many matches found. Please enter a more specific name.');
+  }
 }
 
 function onFetchError() {
   Notify.failure('Oops, there is no country with that name');
 }
-
-// function manyCountries() {
-//     if()
-// }
